4da1b0b74f6575b9c8daa506a92073da3803add0
mario <lopezadeh@gmail.com>
2019-11-04 20:25:20 +1100

    InteractiveApiControllerBase inherits from UmbracoApiControllerBase

--- src/uSlack/Interactive/InteractiveControllerBase.cs
+++ src/uSlack/Interactive/InteractiveControllerBase.cs
@@ -1,16 +1,20 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Web.Http.Controllers;
-using Umbraco.Web.Mvc;
+﻿using Umbraco.Core;
+using Umbraco.Core.Cache;
+using Umbraco.Core.Configuration;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Services;
+using Umbraco.Web;
 using Umbraco.Web.WebApi;
 
 namespace uSlack.Interactive
 {
-    public abstract class InteractiveApiControllerBase : UmbracoApiController
-    {       
+    public abstract class InteractiveApiControllerBase : UmbracoApiControllerBase
+    {
+        public InteractiveApiControllerBase(IGlobalSettings globalSettings, IUmbracoContextAccessor umbracoContextAccessor, ISqlContext sqlContext, ServiceContext services, AppCaches appCaches, IProfilingLogger logger, IRuntimeState runtimeState, UmbracoHelper umbracoHelper)
+            : base(globalSettings, umbracoContextAccessor, sqlContext, services, appCaches, logger, runtimeState, umbracoHelper)
+        {
+
+        }
     }
 }
